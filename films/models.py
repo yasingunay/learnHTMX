6883dd9ff8b965f52fcc9a78683788a95f0584a2
yasingunay <yasingunaydev@gmail.com>
2023-11-30 16:47:55 +0100

    up

--- films/models.py
+++ films/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.functions import Lower
+
 
 class User(AbstractUser):
     pass
@@ -9,5 +11,6 @@ class Film(models.Model):
     name = models.CharField(max_length=128, unique=True)
     users = models.ManyToManyField(User, related_name="films") # each film can be associated with multiple users through the users field #user.films.all()
 
-
+    class Meta:
+        ordering = [Lower('name')]
   
